2c228ce72e7bd7c08912fefbc7c5fed696a386e8
Ularbek <ularbek.zairbekov@iaau.edu.kg>
2021-01-22 13:38:27 +0100

    Added to the project Leaflet map.

--- src/app/shared/custom-components/map/map.component.ts
+++ src/app/shared/custom-components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-
+  map:any
   constructor() { }
 
   ngOnInit(): void {
+    this.map = L.map('map', {
+      center: [ 53.902284, 27.561831 ],
+      zoom: 12
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
