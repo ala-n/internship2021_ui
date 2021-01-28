b3e65a330e266e380358809c3cc033ed3769ff4c
AlexanderBazukevich <alex.baz88@gmail.com>
2021-01-28 22:11:02 +0300

    changed: positioning and animation of map and list components; fixed: removed button focus stylig; removed unnesessary service

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
-import { debounce } from 'rxjs/operators';
-import { ResizeService } from 'src/app/shared/services/resize.service';
 
 @Component({
   selector: 'app-home-page',
@@ -10,36 +7,6 @@ import { ResizeService } from 'src/app/shared/services/resize.service';
 })
 export class HomePageComponent {
   isListVisible = true;
-  fullScreen!: boolean;
-  viewSize!: number;
-
-  private resizeSubscription!: Subscription;
-
-  constructor(private resizeService: ResizeService) {}
-
-  ngOnInit(): void {
-    this.resizeSubscription = this.resizeService.onResize$
-      .pipe(debounce(() => interval(100)))
-      .subscribe((size) => {
-        this.viewSize = size.innerWidth;
-        this.showFullScreen();
-      });
-  }
-
-  ngOnDestroy(): void {
-    if (this.resizeSubscription) {
-      this.resizeSubscription.unsubscribe();
-    }
-  }
-
-  showFullScreen(): void {
-    if (this.viewSize > 1023) {
-      this.fullScreen = false;
-      this.isListVisible = true;
-    } else {
-      this.fullScreen = false;
-    }
-  }
 
   toggleListView(): void {
     this.isListVisible = this.isListVisible ? false : true;
