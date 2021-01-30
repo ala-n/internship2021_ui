84bf134382c55e8743ad3df36e6bb6c2d6039501
AlexanderBazukevich <alex.baz88@gmail.com>
2021-01-30 17:53:07 +0300

    added: language and location components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './custom-components/toolbar/toolbar.component';
 import { MapComponent } from './custom-components/map/map.component';
 import { MaterialModule } from './material-components/material.module';
+import { LocationComponent } from './custom-components/location/location.component';
+import { LanguageComponent } from './custom-components/language/language.component';
 
 @NgModule({
-  declarations: [ToolbarComponent, MapComponent],
+  declarations: [
+    LocationComponent,
+    LanguageComponent,
+    MapComponent,
+    ToolbarComponent
+  ],
   imports: [CommonModule, MaterialModule],
-  exports: [ToolbarComponent, MapComponent, MaterialModule]
+  exports: [
+    LocationComponent,
+    LanguageComponent,
+    MapComponent,
+    ToolbarComponent,
+    MaterialModule
+  ]
 })
 export class SharedModule {}
