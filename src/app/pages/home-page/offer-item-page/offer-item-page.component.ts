e1790284e972a2c9fceeb697ab866008eadf16de
Anna Barmina <anutik-barmina@mail.ru>
2021-02-03 12:11:28 +0100

    implemented item-page flow

--- src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
+++ src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Offer } from 'src/app/shared/models/offer';
+import { OfferService } from 'src/app/shared/services/offer.service';
 
 @Component({
   selector: 'app-offer-item-page',
   templateUrl: './offer-item-page.component.html',
   styleUrls: ['./offer-item-page.component.scss']
 })
-export class OfferItemPageComponent {}
+export class OfferItemPageComponent implements OnInit {
+  offer!: Offer;
+
+  constructor(
+    private route: ActivatedRoute,
+    private readonly offerService: OfferService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.offer = this.offerService.getOfferById(Number(params['id']));
+    });
+  }
+}
