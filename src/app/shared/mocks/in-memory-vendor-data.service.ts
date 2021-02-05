1258ea1d9e8251c0d27da99b21a2c01f07174365
AlexanderBazukevich <alex.baz88@gmail.com>
2021-02-05 21:42:07 +0300

    changed: mock data structure

--- src/app/shared/mocks/in-memory-vendor-data.service.ts
+++ src/app/shared/mocks/in-memory-vendor-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Vendor } from '../models/vendor';
+import vendors from './vendors';
 
 @Injectable({
   providedIn: 'root'
@@ -8,62 +9,6 @@ import { Vendor } from '../models/vendor';
 export class InMemoryVendorDataService implements InMemoryDbService {
   // eslint-disable-next-line  @typescript-eslint/explicit-module-boundary-types
   createDb() {
-    const vendors = [
-      {
-        id: 1,
-        name: 'KFC',
-        title: '',
-        description: '',
-        website: 'www.kfc.by',
-        updated: '23.01.21',
-        isActive: true
-      },
-      {
-        id: 2,
-        name: '24-fitness',
-        title: '',
-        description: '',
-        website: 'www.fit24.by',
-        updated: '22.01.21',
-        isActive: true
-      },
-      {
-        id: 3,
-        name: 'BeautyBox',
-        title: '',
-        description: '',
-        website: 'www.Bbox.by',
-        updated: '24.01.21',
-        isActive: true
-      },
-      {
-        id: 4,
-        name: 'Твой велик',
-        title: '',
-        description: '',
-        website: 'www.twoj-velik.by',
-        updated: '26.01.21',
-        isActive: true
-      },
-      {
-        id: 5,
-        name: 'Wildberries',
-        title: '',
-        description: '',
-        website: 'www.wildberries.com',
-        updated: '26.01.21',
-        isActive: true
-      },
-      {
-        id: 6,
-        name: '21vek',
-        title: '',
-        description: '',
-        website: 'www.21vek.by',
-        updated: '28.01.21',
-        isActive: true
-      }
-    ];
     return { vendors };
   }
 
---
+++ src/app/shared/mocks/vendors.ts
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 1,
+    name: 'KFC',
+    title: '',
+    description: '',
+    website: 'www.kfc.by',
+    updated: '23.01.21',
+    isActive: true
+  },
+  {
+    id: 2,
+    name: '24-fitness',
+    title: '',
+    description: '',
+    website: 'www.fit24.by',
+    updated: '22.01.21',
+    isActive: true
+  },
+  {
+    id: 3,
+    name: 'BeautyBox',
+    title: '',
+    description: '',
+    website: 'www.Bbox.by',
+    updated: '24.01.21',
+    isActive: true
+  },
+  {
+    id: 4,
+    name: 'Твой велик',
+    title: '',
+    description: '',
+    website: 'www.twoj-velik.by',
+    updated: '26.01.21',
+    isActive: true
+  },
+  {
+    id: 5,
+    name: 'Wildberries',
+    title: '',
+    description: '',
+    website: 'www.wildberries.com',
+    updated: '26.01.21',
+    isActive: true
+  },
+  {
+    id: 6,
+    name: '21vek',
+    title: '',
+    description: '',
+    website: 'www.21vek.by',
+    updated: '28.01.21',
+    isActive: true
+  }
+];
