2aa986edc1772495c8556395e004bbaa81a0d111
Anna Barmina <52083788+abarmina@users.noreply.github.com>
2021-02-15 20:56:56 +0100

    Offer card (#44)
    * limiting text to 2 lines
    
    * added styles to tabs
    
    * added router navigation to home page
    
    * restyled offer-item
    
    * rcard link moved
    
    * small edit from Alexey
    
    * first version offer-item-page
    
    * added new bacground
    
    * removed unnecessary div
    
    * added styles for addresses
    
    * added comments

--- src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
+++ src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Offer } from '@shared/models/offer';
+import { MapService } from '@shared/services/map.service';
 import { OfferService } from '@shared/services/offer.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { skip, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-offer-item-page',
   templateUrl: './offer-item-page.component.html',
   styleUrls: ['./offer-item-page.component.scss']
 })
-export class OfferItemPageComponent implements OnInit {
+export class OfferItemPageComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<boolean> = new Subject<boolean>();
   offer$!: Observable<Offer>;
 
   constructor(
     private route: ActivatedRoute,
-    private readonly offerService: OfferService
+    private router: Router,
+    private readonly offerService: OfferService,
+    private readonly mapService: MapService
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
+    this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
       this.offer$ = this.offerService.getOfferById(Number(params['id']));
     });
+
+    this.mapService.city$
+      .pipe(skip(1), takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.router.navigate(['/home']);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 }
