5d48d519df5d379d8301cdb233d8f7cb5d7f84a1
Taras <taraslaz10@ukr.net>
2021-02-23 18:47:54 +0200

    Upgrade our login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@shared/services/auth.service';
 import { LoginData } from '@shared/models/login_data';
-import { HttpService } from '@shared/services/http.service';
 
 @Component({
   selector: 'app-login',
@@ -9,20 +8,14 @@ import { HttpService } from '@shared/services/http.service';
   styleUrls: ['./login-page.component.scss']
 })
 export class LogInComponent {
-  // Old function to login without any authentication
-  // signInSubmit(event: Event): void {
-  //   event.preventDefault();
-  // }
-
   model: LoginData = {};
-  constructor(private authService: AuthService, private http: HttpService) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.authService.logout();
   }
 
   login() {
-    // this.model.action = 'login';
     this.authService.loginForm(this.model).subscribe(
       (response) => {
         if (response.token != null) {
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -4,9 +4,9 @@ export interface User {
   _id: ObjectID;
   firstName: string;
   lastName: string;
-  photoUrl: null,
-  isActive: false,
-  city: null,
-  role: 0,
+  photoUrl: null;
+  isActive: false;
+  city: null;
+  role: 0;
   token: string;
 }
