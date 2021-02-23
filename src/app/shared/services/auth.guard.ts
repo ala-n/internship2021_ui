09c72adcf3eee3a590b1bf64975e4e26bcaeb652
Taras <taraslaz10@ukr.net>
2021-02-23 19:41:47 +0200

    Add comments how use with backend

--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { AuthService } from '@shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    // private router: Router, // uncomment this to use with backend
+    private authService: AuthService
+  ) {}
 
   canActivate(): boolean {
     if (!this.authService.isLoggedIn()) {
-      //this.router.navigate(['/login']);
+      // this.router.navigate(['/login']); // uncomment this to use with backend
       return true; // should be false
     } else {
       return true;
