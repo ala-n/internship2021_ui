fe352daa3109906cd2bad68a9b7e902d683aa0d0
AlexanderBazukevich <alex.baz88@gmail.com>
2021-03-02 08:58:41 +0300

    added: paths to back endpoits

--- src/app/shared/services/vendor.service.ts
+++ src/app/shared/services/vendor.service.ts
@@ -24,21 +24,24 @@ export class VendorService {
       );
   }
 
+  getVendorsAllAdmin(): Observable<Vendor> {
+    const url = `${VendorService.VENDORS_URL}/allAdmin`;
+    return this.http.get<Vendor>(url);
+  }
+
   getVendorById(id: string): Observable<Vendor> {
+    // const url = `${VendorService.VENDORS_URL}/get/${id}`;
     const url = `${VendorService.VENDORS_URL}/${id}`;
     return this.http.get<Vendor>(url);
   }
 
   addVendor(vendor: Vendor): Observable<Vendor> {
-    return this.http.post<Vendor>(
-      VendorService.VENDORS_URL,
-      vendor,
-      this.httpOptions
-    );
+    // const url = `${VendorService.VENDORS_URL}/add`;
+    const url = VendorService.VENDORS_URL;
+    return this.http.post<Vendor>(url, vendor, this.httpOptions);
   }
 
   updateVendor(vendor: Vendor, vendorId: string): Observable<Vendor> {
-    console.log(vendor, vendorId);
     const url = `${VendorService.VENDORS_URL}/${vendorId}`;
     return this.http.put<Vendor>(url, vendor, this.httpOptions);
   }
