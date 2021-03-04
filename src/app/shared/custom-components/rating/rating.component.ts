2813240597ea923864d7048f58b4ce31310ca01f
Anna Barmina <anutik-barmina@mail.ru>
2021-03-04 11:05:46 +0100

    data transfer to the ating component

--- src/app/shared/custom-components/rating/rating.component.ts
+++ src/app/shared/custom-components/rating/rating.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.scss']
 })
-export class RatingComponent {}
+export class RatingComponent {
+  @Input() rating!: number;
+}
