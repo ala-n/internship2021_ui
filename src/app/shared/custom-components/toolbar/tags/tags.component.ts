f9e80c91768a7f90453b0ac7d606777fd6811ad5
Zairbekov <Zairbekov@users.noreply.github.com>
2021-03-04 11:26:54 +0100

    Refactoring services (#77)
    * Upgrade version(npm install bson)
    
    * Npm install some kind of leafmap
    
    * Write tag, when you click
    
    * added filtering by tags
    
    * fixed any type
    
    * filter service
    
    * Fixed Filter
    
    * Fixed return types
    
    * Void
    
    Co-authored-by: Taras <taraslaz10@ukr.net>
    Co-authored-by: Anna Barmina <anutik-barmina@mail.ru>

--- src/app/shared/custom-components/toolbar/tags/tags.component.ts
+++ src/app/shared/custom-components/toolbar/tags/tags.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Tag } from '@shared/models/tag';
+import { FilterService } from '@shared/services/filter.service';
 import { TagsService } from '@shared/services/tag.service';
 
 @Component({
@@ -11,7 +12,10 @@ import { TagsService } from '@shared/services/tag.service';
 export class TagsComponent implements OnInit {
   tags!: Tag[];
 
-  constructor(private tagsService: TagsService) {}
+  constructor(
+    private tagsService: TagsService,
+    private filterService: FilterService
+  ) {}
 
   ngOnInit(): void {
     this.tagsService
@@ -21,5 +25,15 @@ export class TagsComponent implements OnInit {
 
   receiveData(data: Tag[]): void {
     this.tags = data;
+  }
+
+  onClick(e: Event): void {
+    const target = e.target as HTMLElement;
+    const tag = (target.textContent || '').trim();
+    if (tag === this.filterService.filterCfg.tag) {
+      this.filterService.filter({ tag: '' });
+    } else {
+      this.filterService.filter({ tag });
+    }
   }
 }
