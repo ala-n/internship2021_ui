00b2ed924f5f6dde5ac580128f874f09b1c13206
Anna Barmina <anutik-barmina@mail.ru>
2021-03-07 18:58:43 +0100

    added space

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -11,6 +11,7 @@ import { VendorItemPageComponent } from './vendor-item-page/vendor-item-page.com
 import { OfficeItemPageComponent } from './office-item-page/office-item-page.component';
 
 import { ConcatPipe } from '@shared/pipes/concat.pipe';
+
 @NgModule({
   declarations: [
     HomePageComponent,
