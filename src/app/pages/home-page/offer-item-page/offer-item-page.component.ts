c4d17113cc4147495311b189d4a2b0b6d52a20ce
Ularbek <ularbek.zairbekov@iaau.edu.kg>
2021-03-09 22:50:33 +0100

    favorite offer

--- src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
+++ src/app/pages/home-page/offer-item-page/offer-item-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Offer } from '@shared/models/offer';
 import { Office } from '@shared/models/office';
+import { FavoriteOfferService } from '@shared/services/favorite-offer.service';
 import { MapService } from '@shared/services/map.service';
 import { OfferService } from '@shared/services/offer.service';
 import { of } from 'rxjs';
@@ -16,18 +17,24 @@ import { map, switchMap, tap } from 'rxjs/operators';
 export class OfferItemPageComponent implements OnInit, OnDestroy {
   offer!: Offer;
   offices!: Office[];
-
+  offerId!: string;
+  favoriteIs!: boolean;
   isLoading$ = of(true);
 
   constructor(
     private route: ActivatedRoute,
     private readonly offerService: OfferService,
-    private readonly mapService: MapService
+    private readonly mapService: MapService,
+    private favoriteOfferService: FavoriteOfferService
   ) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.route.params.pipe(
-      switchMap((params) => this.offerService.getOfferById(params['id'])),
+      switchMap((params) => {
+        this.offerId = params['id'];
+        this.isFavorited(this.offerId);
+        return this.offerService.getOfferById(params['id']);
+      }),
       tap((offer: Offer) => {
         this.offer = offer;
         this.mapService.setOffer(offer);
@@ -38,6 +45,29 @@ export class OfferItemPageComponent implements OnInit, OnDestroy {
       // for backend
       map(() => false)
     );
+  }
+
+  addFavorite(): void {
+    this.favoriteOfferService
+      .addFavoriteOffer(this.offerId)
+      .subscribe((offer) => {
+        if (offer.offerId) this.isFavorited(offer.offerId);
+      });
+  }
+
+  deleteFavorite(): void {
+    this.favoriteOfferService
+      .deleteFavorite(this.offerId)
+      .subscribe((offer) => {
+        if (offer !== {}) this.isFavorited(this.offerId);
+      });
+  }
+
+  isFavorited(id: string): void {
+    this.favoriteOfferService.isFavoriteOffer(id).subscribe((offer) => {
+      if (offer == null) this.favoriteIs = false;
+      else this.favoriteIs = true;
+    });
   }
 
   ngOnDestroy(): void {
