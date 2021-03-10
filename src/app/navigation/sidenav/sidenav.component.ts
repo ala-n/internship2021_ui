e007afa12d5918fdba299c4779d9b518a36747ef
Anna Barmina <anutik-barmina@mail.ru>
2021-03-10 11:33:59 +0100

    aadded links to history and bookmark pages

--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationService } from '@shared/services/navigation.service';
 import { AuthService } from '@shared/services/auth.service';
+import { LocationService } from '@shared/services/location.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -10,7 +11,8 @@ import { AuthService } from '@shared/services/auth.service';
 export class SidenavComponent {
   constructor(
     public navigationService: NavigationService,
-    private httpAuth: AuthService
+    private httpAuth: AuthService,
+    public locationService: LocationService
   ) {}
 
   logout(): void {
