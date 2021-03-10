f997cbd41c08f833720a97e82aebe281524721f2
Anna Barmina <anutik-barmina@mail.ru>
2021-03-10 09:40:24 +0100

    added form to dialog

--- src/app/pages/home-page/offer-item-page/pre-order-dialog/pre-order-dialog.component.ts
+++ src/app/pages/home-page/offer-item-page/pre-order-dialog/pre-order-dialog.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { Office } from '@shared/models/office';
+import { Preorder } from '@shared/models/preorder';
 import { User } from '@shared/models/user';
-import { UserService } from '@shared/services/user.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pre-order-dialog',
   templateUrl: './pre-order-dialog.component.html',
   styleUrls: ['./pre-order-dialog.component.scss']
 })
-export class PreOrderDialogComponent implements OnInit {
-  user$!: Observable<User | null>;
+export class PreOrderDialogComponent {
+  model: Preorder = {
+    name: `${this.data.user.firstName} ${this.data.user.lastName}`,
+    phone:
+      this.data.user.phone === '' ? this.data.user.phone : '+7 950-64-34-190',
+    comment: 'promocode'.toLowerCase() + ': ' + this.data.promocode,
+    offerId: this.data.offerId
+  };
 
-  constructor(private userService: UserService) {}
-  ngOnInit(): void {
-    this.user$ = this.userService.user$;
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      promocode: string;
+      offices: Office[];
+      user: User;
+      offerId: string;
+    }
+  ) {}
+
+  sendPreorder(): void {
+    console.log('your pre-order has been sent to the brand mail', this.model);
   }
 }
